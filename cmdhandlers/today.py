549469e68245c57b8ad639f6c42bcbeab004dda7
JMBalanzar <45176438+SrGitQ@users.noreply.github.com>
2022-05-13 23:22:36 -0500

    Update today.py

--- cmdhandlers/today.py
+++ cmdhandlers/today.py
@@ -2,7 +2,7 @@
 def today(update, context):
     #logic
     logic_text = ''
-    text = f'Helloo, good day Dinamikos {logic_text} Have a nice day!'
+    text = f'Helloo, good day Dinamikos\n{logic_text}\nHave a nice day!'
 
     chat_id = update.message.chat_id
     context.bot.send_message(chat_id, text=text)
